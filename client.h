1813795d45e6e5ea6870e0422c0cd713c3e8b644
Pedro Vanzella <pedro@pedrovanzella.com>
2016-01-21 17:06:16 -0200

    Classe Client. Forward-decl quebrada

--- client.h
+++ client.h
@@ -1,8 +1,13 @@
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
 
+//#include "floor.h"
+
 class Client {
-  
+public:
+  int partySize;
+  double arrivalTime;
+  //  Floor destination; // Need to fix forward-decl
 };
 
 #endif /* _CLIENT_H_ */
