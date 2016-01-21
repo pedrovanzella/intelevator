f604a7afadeac3262e32fb9f4a72865a3b29abde
Pedro Vanzella <pedro@pedrovanzella.com>
2016-01-21 16:56:55 -0200

    Stubs para Floor

--- floor.h
+++ floor.h
@@ -1,8 +1,14 @@
 #ifndef _FLOOR_H_
 #define _FLOOR_H_
 
+#include <queue>
+#include "client.h"
+
 class Floor {
-  
+public:
+  int number;
+  std::queue<Client> upLine;
+  std::queue<Client> downLine;
 };
 
 #endif /* _FLOOR_H_ */
