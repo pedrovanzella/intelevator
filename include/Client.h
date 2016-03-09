5b268efc95f15cef8c59e7c4ebb2e7e9bb1a6a13
Guilherme Taschetto <gtaschetto@gmail.com>
2016-03-08 22:01:00 -0300

    Add Client class implementation.

--- include/Client.h
+++ include/Client.h
@@ -3,10 +3,15 @@
 class Floor; // forward declaration
 
 class Client {
-public:
-  int partySize;
-  double arrivalTime;
-  Floor* destination;
+private:
+  const int partySize;
+  const float arrivalTime;
+  const Floor& destination;
 
-  Client();
+public:
+  Client(const int, const float, const Floor&);
+
+  const int getPartySize();
+  const float getArrivalTime();
+  const Floor& getDestination();
 };
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,3 +1,22 @@
 #include "Client.h"
 
-Client::Client() {}
+Client::Client(int partySize, float arrivalTime, const Floor& destination)
+  : partySize(partySize)
+  , arrivalTime(arrivalTime)
+  , destination(destination)
+{}
+
+const int Client::getPartySize()
+{
+  return this->partySize;
+}
+
+const float Client::getArrivalTime()
+{
+  return this->arrivalTime;
+}
+
+const Floor& Client::getDestination()
+{
+  return this->destination;
+}
