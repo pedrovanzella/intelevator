81a1c6c8e524ddf412a39b6a9c15b311af8a1f71
Guilherme Taschetto <gtaschetto@gmail.com>
2016-03-09 22:09:15 -0300

    Update Statistics class.

--- include/Statistics.h
+++ include/Statistics.h
@@ -2,13 +2,21 @@
 
 #include <list>
 #include "Config.h"
+#include "EventObserver.h"
 #include "Trip.h"
 
-class Statistics {
+class Statistics : public EventObserver
+{
 public:
+  Statistics(Config&);
+
+  void reset(Config& config);
+  bool keepRunning();
+
+  void notify(const Event&);
+
   std::list<Trip*> trips;
 
-  Statistics();
-  void reset(Config config);
-  bool keepRunning();
+private:
+  Config& config;
 };
--- src/Statistics.cpp
+++ src/Statistics.cpp
@@ -1,12 +1,20 @@
 #include "Statistics.h"
 
-Statistics::Statistics()
+Statistics::Statistics(Config& config)
+ : config(config)
 {}
 
-void Statistics::reset(Config config)
-{}
+void Statistics::reset(Config& config)
+{
+  this->config = config;
+}
 
 bool Statistics::keepRunning()
 {
   return true;
+}
+
+void Statistics::notify(const Event& event)
+{
+  // Handle events
 }
