f75c3e2f39e47b14381a2c12cdda7ba99ab4445f
Pedro Vanzella <pedro@pedrovanzella.com>
2016-03-22 19:44:51 -0300

    Implement NearestNeighbourDispatcher

--- src/NearestNeighbourDispatcher.cpp
+++ src/NearestNeighbourDispatcher.cpp
@@ -9,12 +9,21 @@
 std::shared_ptr<const Elevator> NearestNeighbourDispatcher::pick_next_elevator(const std::shared_ptr<const ClientArrival> ca) const
 {
     auto elevators = _building->getElevators();
-    auto floor = ca->getFloor();
+    auto request_floor = ca->getFloor();
 
     auto closest = elevators.front();
 
     for (auto elevator : elevators) {
-        // get the current floor for the elevator
+        auto where_it_is = elevator->getCurrentFloor()->getNumber();
+        auto where_to = request_floor->getNumber();
+        auto where_the_closest_is = closest->getCurrentFloor()->getNumber();
+
+        auto distance = where_it_is - where_to;
+        auto closest_distance = where_the_closest_is - where_to;
+
+        if (distance < closest_distance) {
+            closest = elevator;
+        }
     }
 
     return closest;
