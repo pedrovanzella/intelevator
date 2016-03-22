e128bab75e133c69debc2966efd7f190c1b6d66d
Pedro Vanzella <pedro@pedrovanzella.com>
2016-03-22 19:09:14 -0300

    Add Floor to ClientArrival

--- include/ClientArrival.h
+++ include/ClientArrival.h
@@ -8,9 +8,11 @@
 class ClientArrival : public Event
 {
 public:
-  ClientArrival(const float eventTime, const std::shared_ptr<const Client> client);
+  ClientArrival(const float eventTime, const std::shared_ptr<const Client> client, const std::shared_ptr<const Floor> floor);
   const std::shared_ptr<const Client> getClient() const;
+    const std::shared_ptr<const Floor> getFloor() const;
 
 private:
   const std::shared_ptr<const Client> _client;
+    const std::shared_ptr<const Floor> _floor;
 };
