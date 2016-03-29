74d211ea7f519233e74e95bdf832237630b371d4
Guilherme Taschetto <gtaschetto@gmail.com>
2016-03-28 21:54:09 -0300

    Fix style and indentation.

--- include/MissingCostFunctionError.h
+++ include/MissingCostFunctionError.h
@@ -1,18 +1,9 @@
-//
-// Created by Pedro Vanzella on 3/28/16.
-//
-
-#ifndef INTELEVATOR_MISSINGCOSTFUNCTIONERROR_H
-#define INTELEVATOR_MISSINGCOSTFUNCTIONERROR_H
-
+#pragma once
 #include <stdexcept>
 
 class MissingCostFunctionError : public std::runtime_error
 {
 public:
-    explicit MissingCostFunctionError(const std::string& what) : std::runtime_error("Cost Function not found: " + what) {}
-
-    virtual ~MissingCostFunctionError() {}
+  explicit MissingCostFunctionError(const std::string& what) : std::runtime_error("Cost Function not found: " + what) {}
+  virtual ~MissingCostFunctionError() {}
 };
-
-#endif //INTELEVATOR_MISSINGCOSTFUNCTIONERROR_H
--- include/MissingDispatcherError.h
+++ include/MissingDispatcherError.h
@@ -1,18 +1,9 @@
-//
-// Created by Pedro Vanzella on 3/28/16.
-//
-
-#ifndef INTELEVATOR_MISSINGDISPATCHERERROR_H
-#define INTELEVATOR_MISSINGDISPATCHERERROR_H
-
+#pragma once
 #include <stdexcept>
 
 class MissingDispatcherError : public std::runtime_error
 {
 public:
-    explicit MissingDispatcherError(const std::string& what) : std::runtime_error("Dispatcher not found: " + what) {}
-
-    virtual ~MissingDispatcherError() {}
+  explicit MissingDispatcherError(const std::string& what) : std::runtime_error("Dispatcher not found: " + what) {}
+  virtual ~MissingDispatcherError() {}
 };
-
-#endif //INTELEVATOR_MISSINGDISPATCHERERROR_H
