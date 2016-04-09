04414937b229dbc01cda0be026795beab831a2d8
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-09 17:13:35 -0300

    Fix indentation.

--- src/Building.cpp
+++ src/Building.cpp
@@ -8,12 +8,12 @@
 #include <glog/logging.h>
 
 Building::Building(
-    std::shared_ptr<std::vector<std::shared_ptr<Floor>>> floors,
-    std::shared_ptr<std::vector<std::shared_ptr<Elevator>>> elevators,
-    std::shared_ptr<const Dispatcher> dispatcher,
-    std::shared_ptr<const CostFunction> costFunction)
-    : _floors(floors), _elevators(elevators), _dispatcher(dispatcher),
-      _costFunction(costFunction) {}
+  std::shared_ptr<std::vector<std::shared_ptr<Floor>>> floors,
+  std::shared_ptr<std::vector<std::shared_ptr<Elevator>>> elevators,
+  std::shared_ptr<const Dispatcher> dispatcher,
+  std::shared_ptr<const CostFunction> costFunction)
+  : _floors(floors), _elevators(elevators), _dispatcher(dispatcher),
+    _costFunction(costFunction) {}
 
 Building::~Building() {}
 
