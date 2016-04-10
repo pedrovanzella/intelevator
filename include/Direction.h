cfbcfd20de61efc48e804f57079b926c7be00199
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-09 22:22:05 -0300

    Add helper for printing a Direction name.

--- include/Direction.h
+++ include/Direction.h
@@ -5,3 +5,18 @@ enum class Direction {
   Down,
   None
 };
+
+namespace Helpers
+{
+  static std::string directionName(Direction type)
+  {
+    switch(type)
+    {
+      case Direction::Up:   return "Up";
+      case Direction::Down: return "Down";
+      case Direction::None: return "None";
+      default:
+        throw std::invalid_argument("");
+    }
+  }
+}
