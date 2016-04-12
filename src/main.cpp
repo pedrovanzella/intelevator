c38c4cbccac978f05eba7b723099956a392e707f
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-12 20:47:05 -0300

    Make simulator a shared object.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
-#include <memory>
-#include <vector>
-#include <glog/logging.h>
 #include "Scenario.h"
 #include "Simulator.h"
+#include <glog/logging.h>
+#include <memory>
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   FLAGS_logtostderr = true;
   google::InitGoogleLogging(argv[0]);
 
   auto scenarios = Scenario::Load("config.yaml");
-  for (auto scenario : *scenarios)
-  {
-    Simulator sim(scenario);
-    sim.run();
+  for (auto scenario : *scenarios) {
+    auto simulator = std::make_shared<Simulator>(scenario);
+    simulator->run();
   }
 
   return 0;
