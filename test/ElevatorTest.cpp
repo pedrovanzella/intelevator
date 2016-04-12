1aa1f9399c032363f08d0168c0d0b3ec012cff15
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-12 00:48:25 -0300

    Fix tests.

--- test/ElevatorTest.cpp
+++ test/ElevatorTest.cpp
@@ -52,58 +52,53 @@ TEST_F(ElevatorTest, GetDestination)
 
 TEST_F(ElevatorTest, MoveUp)
 {
-  EXPECT_EQ(elevator->getLocation(), location);
-  EXPECT_EQ(elevator->getStatus(), Status::Stopped);
+  EXPECT_EQ(location, elevator->getLocation());
+  EXPECT_EQ(Status::Idle, elevator->getStatus());
 
   elevator->setDestination(destination);
-  EXPECT_EQ(elevator->getDestination(), destination);
-
-  elevator->start();
-
-  EXPECT_EQ(elevator->getDirection(), Direction::Up);
+  EXPECT_EQ(destination, elevator->getDestination());
+  EXPECT_EQ(Status::Moving, elevator->getStatus());
+  EXPECT_EQ(Direction::Up, elevator->getDirection());
 
   for (int i = location; i < destination - 1; i++)
   {
     elevator->update();
-    EXPECT_EQ(elevator->getLocation(), i + 1);
-    EXPECT_EQ(elevator->getStatus(), Status::Moving);
+    EXPECT_EQ(i + 1, elevator->getLocation());
+    EXPECT_EQ(Status::Moving, elevator->getStatus());
   }
 
   elevator->update();
-  EXPECT_EQ(elevator->getLocation(), location + destination);
+  EXPECT_EQ(location + destination, elevator->getLocation());
 
   elevator->stop();
-  EXPECT_EQ(elevator->getStatus(), Status::Stopped);
+  EXPECT_EQ(Status::Stopped, elevator->getStatus());
 }
 
 TEST_F(ElevatorTest, MoveDown)
 {
   location = 5;
   elevator->setLocation(location);
-  EXPECT_EQ(elevator->getLocation(), location);
+  EXPECT_EQ(location, elevator->getLocation());
+  EXPECT_EQ(Status::Idle, elevator->getStatus());
 
   destination = 0;
   elevator->setDestination(destination);
-  EXPECT_EQ(elevator->getDestination(), destination);
-
-  elevator->turn();
-  elevator->start();
-
-  EXPECT_EQ(elevator->getDirection(), Direction::Down);
-  EXPECT_EQ(elevator->getStatus(), Status::Moving);
+  EXPECT_EQ(destination, elevator->getDestination());
+  EXPECT_EQ(Status::Moving, elevator->getStatus());
+  EXPECT_EQ(Direction::Down, elevator->getDirection());
 
   for (int i = location; i > destination + 1; i--)
   {
     elevator->update();
-    EXPECT_EQ(elevator->getLocation(), i - 1);
-    EXPECT_EQ(elevator->getStatus(), Status::Moving);
+    EXPECT_EQ(i - 1, elevator->getLocation());
+    EXPECT_EQ(Status::Moving, elevator->getStatus());
   }
 
   elevator->update();
-  EXPECT_EQ(elevator->getLocation(), destination);
+  EXPECT_EQ(destination, elevator->getLocation());
 
   elevator->stop();
-  EXPECT_EQ(elevator->getStatus(), Status::Stopped);
+  EXPECT_EQ(Status::Stopped, elevator->getStatus());
 }
 
 TEST_F(ElevatorTest, DropPassengersToCurrentLocation)
