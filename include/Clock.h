d22510cdaa8fd7d0558fffc7416c57749e6c7416
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-12 21:08:40 -0300

    Restyle and refactory.

--- include/Clock.h
+++ include/Clock.h
@@ -1,18 +1,17 @@
 #pragma once
 
-#include <memory>
 #include "EventObserver.h"
+#include <memory>
 
-class Clock : public EventObserver
-{
+class Clock : public EventObserver {
 public:
   Clock();
   virtual ~Clock();
 
   unsigned long currentTime() const;
+
   void advanceTo(const unsigned long time);
   void advanceBy(const unsigned long amount);
-
   void notify(const std::shared_ptr<const Event> event);
 
 private:
--- src/Clock.cpp
+++ src/Clock.cpp
@@ -1,31 +1,24 @@
 #include "Clock.h"
 #include <glog/logging.h>
 
-Clock::Clock()
- : _time(0)
-{}
+Clock::Clock() : _time(0) {}
 
-Clock::~Clock()
-{}
+Clock::~Clock() {}
 
-unsigned long Clock::currentTime() const
-{
-  return _time;
-}
+unsigned long Clock::currentTime() const { return _time; }
 
-void Clock::advanceTo(const unsigned long time)
-{
-  if (time <= _time) return;
+void Clock::advanceTo(const unsigned long time) {
+  if (time <= _time)
+    return;
   _time = time;
 }
 
-void Clock::advanceBy(const unsigned long amount)
-{
-  if (amount <= 0) return;
+void Clock::advanceBy(const unsigned long amount) {
+  if (amount <= 0)
+    return;
   _time += amount;
 }
 
-void Clock::notify(const std::shared_ptr<const Event> event)
-{
-  // Handle events
+void Clock::notify(const std::shared_ptr<const Event> event) {
+  advanceTo(event->getTime());
 }
