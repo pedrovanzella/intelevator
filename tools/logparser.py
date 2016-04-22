4b7fce684118abd01e803437da05dd3aa929140c
Pedro Vanzella <pedro@pedrovanzella.com>
2016-04-22 19:25:41 -0300

    function to extract only the times from the data

--- tools/logparser.py
+++ tools/logparser.py
@@ -7,22 +7,29 @@ import numpy as np
 def makegraphs(filename):
     data = loadfile(filename)
     print(data)
+    times = getTimes(data)
+    print(times)
     plt.plot(data)
     plt.ylabel('some numbers')
     plt.show()
 
 
 def loadfile(filename):
+    # 0: clientID
+    # 1: partySize
+    # 2: elevatorID
+    # 3: arrivalFloor
+    # 4: dropoffFloor'
+    # 5: createTime
+    # 6: pickupTime
+    # 7: dropoffTime
     with open(filename, 'r') as f:
-        data = np.loadtxt(f, dtype={'names':
-                                    ('clientID', 'partySize', 'elevatorID',
-                                     'arrivalFloor', 'dropoffFloor',
-                                     'createTime', 'pickupTime',
-                                     'dropoffTime'),
-                                    'formats':
-                                    ('i4', 'i4', 'i4', 'i4', 'i4',
-                                     'i4', 'i4', 'i4')})
+        data = np.loadtxt(f)
         return data
+
+
+def getTimes(data):
+    return data[:, [5, 6, 7]]
 
 if __name__ == "__main__":
     if len(argv) < 2:
