b93647e8f6fb69ffe2324b09c39d317c42787194
Pedro Vanzella <pedro@pedrovanzella.com>
2016-04-22 17:24:13 -0300

    load data with numpy

--- tools/logparser.py
+++ tools/logparser.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 from sys import argv
 import matplotlib.pyplot as plt
+import numpy as np
 
-def makegraphs(file):
-    plt.plot([1, 2, 3, 4])
+def makegraphs(filename):
+    data = loadfile(filename)
+    print(data)
+    plt.plot(data)
     plt.ylabel('some numbers')
     plt.show()
+
+def loadfile(filename):
+    with open(filename, 'r') as f:
+        data = np.loadtxt(f, dtype={'names':
+                                    ('clientID', 'partySize', 'elevatorID',
+                                     'arrivalFloor', 'dropoffFloor',
+                                     'createTime', 'pickupTime',
+                                     'dropoffTime'),
+                                    'formats':
+                                    ('i4', 'i4', 'i4', 'i4', 'i4',
+                                     'i4', 'i4', 'i4')})
+        return data
 
 if __name__ == "__main__":
     if len(argv) < 2:
