d0c0af06be39788ba05d083e5ffe38da64d1affb
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-23 15:37:08 -0300

    Add seed member to EventFactory class.

--- include/EventFactory.h
+++ include/EventFactory.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <glog/logging.h>
+#include <random>
 
 class EventFactory
 {
 public:
-  EventFactory();
+  EventFactory(const std::string seed);
   virtual ~EventFactory();
+
+private:
+  std::seed_seq _seed_sequence;
 };
--- src/EventFactory.cpp
+++ src/EventFactory.cpp
@@ -1,5 +1,6 @@
 #include "EventFactory.h"
 
-EventFactory::EventFactory() {}
+EventFactory::EventFactory(const std::string seed)
+    : _seed_sequence(seed.begin(), seed.end()) {}
 
 EventFactory::~EventFactory() {}
