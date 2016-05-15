b2f8df4d249944e4f7167045999d841b240dbaf8
Guilherme Taschetto <gtaschetto@gmail.com>
2016-05-15 13:07:02 -0300

    Add setter method for event time.

--- include/Event.h
+++ include/Event.h
@@ -7,15 +7,17 @@ class Event {
 public:
   static unsigned long totalEvents;
 
-  Event(unsigned long time, EventType eventType);
+  Event(unsigned long time, EventType type);
   unsigned long getId() const;
   unsigned long getTime() const;
   EventType getType() const;
+
+  void setTime(unsigned long time);
 
   virtual std::string str() const;
 
 protected:
   unsigned long _id;
-  unsigned long _eventTime;
-  EventType _eventType;
+  unsigned long _time;
+  EventType _type;
 };
