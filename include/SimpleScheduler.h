98d21c00a45b17bfbc62c25c9d2595f35cf0f85b
Guilherme Taschetto <gtaschetto@gmail.com>
2016-05-26 19:44:23 -0300

    Add SimpleScheduler.

---
+++ include/SimpleScheduler.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Scheduler.h"
+
+class Building;      // forward declaration
+class ClientArrival; // forward declaration
+class CostFunction;  // forward declaration
+
+class SimpleScheduler : public Scheduler {
+public:
+  int schedule(const std::shared_ptr<const CostFunction> costFunction,
+               const std::shared_ptr<const Building> building,
+               const std::shared_ptr<const ClientArrival>);
+};
---
+++ src/SimpleScheduler.cpp
@@ -0,0 +1,29 @@
+#include "SimpleScheduler.h"
+#include "Building.h"
+#include "Client.h"
+#include "ClientArrival.h"
+#include "CostFunction.h"
+#include "Elevator.h"
+#include "Floor.h"
+#include <limits>
+
+int SimpleScheduler::schedule(
+    const std::shared_ptr<const CostFunction> costFunction,
+    const std::shared_ptr<const Building> building,
+    const std::shared_ptr<const ClientArrival> ca) {
+
+  auto elevators = building->getElevators();
+
+  float best_cost = std::numeric_limits<float>::infinity();
+  auto the_chosen_one = elevators->front();
+
+  for (auto elevator : *elevators) {
+    auto cost = costFunction->calculate(elevator, ca->getClient());
+    if (cost < best_cost) {
+      best_cost = cost;
+      the_chosen_one = elevator;
+    }
+  }
+
+  return the_chosen_one->getNumber();
+}
