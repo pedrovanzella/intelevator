eb20be43806365229b8bea2627d1fdcee1fd6590
Guilherme Taschetto <gtaschetto@gmail.com>
2016-05-26 21:16:02 -0300

    Remove scheduler and update cost functions.

---
+++ include/BetterNearestNeighbourCostFunction.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CostFunction.h"
+
+class Building; // forward declaration
+class Client;   // forward declaration
+class Elevator; // forward declaration
+
+class BetterNearestNeighbourCostFunction : public CostFunction {
+public:
+  float calculate(const std::shared_ptr<const Building> building,
+                  const std::shared_ptr<const Elevator> elevator,
+                  const std::shared_ptr<const Client> client) const;
+};
--- src/BetterNearestNeighbourCostFunction.cpp
+++ src/BetterNearestNeighbourCostFunction.cpp
@@ -1,10 +1,13 @@
+#include "BetterNearestNeighbourCostFunction.h"
 #include "NearestNeighbourCostFunction.h"
 #include "Building.h"
 #include "Client.h"
 #include "Elevator.h"
 #include "Floor.h"
+#include "Status.h"
+#include <algorithm>
 
-float NearestNeighbourCostFunction::calculate(
+float BetterNearestNeighbourCostFunction::calculate(
     const std::shared_ptr<const Building> building,
     const std::shared_ptr<const Elevator> elevator,
     const std::shared_ptr<const Client> client) const {
@@ -12,6 +15,14 @@ float NearestNeighbourCostFunction::calculate(
   auto where_it_is = elevator->getLocation();
   auto where_to = client->getArrivalFloor();
   auto distance = where_it_is - where_to;
+
+  auto floors = building->getFloors();
+  auto current_floor = floors->at(where_it_is);
+  auto request_floor = floors->at(where_to);
+
+  if (elevator->getDirection() == current_floor->compareTo(*request_floor) || elevator->getStatus() == Status::Idle) {
+    return abs(distance) / sqrt(2.0);
+  }
 
   return abs(distance);
 }
