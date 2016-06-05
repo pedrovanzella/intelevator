299ea5bb773ba5ac2abd5483a655f96f1d1b3612
Guilherme Taschetto <gtaschetto@gmail.com>
2016-06-05 17:48:04 -0300

    UPdate tests.

--- test/StopManagerTest.cpp
+++ test/StopManagerTest.cpp
@@ -15,8 +15,8 @@ struct StopManagerTest : testing::Test {
 TEST_F(StopManagerTest, GetNull) {
   auto f = std::make_shared<Floor>(0, 0);
   auto e = std::make_shared<Elevator>(0, 0, 0);
-  EXPECT_EQ(nullptr, sm->get(f, Direction::Up));
-  EXPECT_EQ(nullptr, sm->get(f, Direction::Down));
+  EXPECT_EQ(false, sm->hasStop(f, Direction::Up));
+  EXPECT_EQ(false, sm->hasStop(f, Direction::Down));
 
   EXPECT_EQ(0, sm->getStops(e, Direction::Up).size());
   EXPECT_EQ(0, sm->getStops(e, Direction::Down).size());
@@ -27,14 +27,14 @@ TEST_F(StopManagerTest, SetUp) {
   auto d = Direction::Up;
   auto e = std::make_shared<Elevator>(0, 0, 0);
   sm->set(f, d, e);
-  EXPECT_EQ(e, sm->get(f, d));
-  EXPECT_EQ(nullptr, sm->get(f, Direction::Down));
+  EXPECT_EQ(true, sm->hasStop(f, d));
+  EXPECT_EQ(false, sm->hasStop(f, Direction::Down));
   auto stops = sm->getStops(e, d);
   EXPECT_EQ(1, stops.size());
   EXPECT_EQ(f, *stops.begin());
 
-  sm->clear(f, d);
-  EXPECT_EQ(nullptr, sm->get(f, d));
+  sm->clear(f, e, d);
+  EXPECT_EQ(false, sm->hasStop(f, d));
 
   stops = sm->getStops(e, d);
   EXPECT_EQ(0, stops.size());
@@ -46,14 +46,14 @@ TEST_F(StopManagerTest, SetDown) {
   auto d = Direction::Down;
   auto e = std::make_shared<Elevator>(0, 0, 0);
   sm->set(f, d, e);
-  EXPECT_EQ(e, sm->get(f, d));
-  EXPECT_EQ(nullptr, sm->get(f, Direction::Up));
+  EXPECT_EQ(true, sm->hasStop(f, d));
+  EXPECT_EQ(false, sm->hasStop(f, Direction::Up));
   auto stops = sm->getStops(e, d);
   EXPECT_EQ(1, stops.size());
   EXPECT_EQ(f, *stops.begin());
 
-  sm->clear(f, d);
-  EXPECT_EQ(nullptr, sm->get(f, d));
+  sm->clear(f, e, d);
+  EXPECT_EQ(false, sm->hasStop(f, d));
 
   stops = sm->getStops(e, d);
   EXPECT_EQ(0, stops.size());
@@ -67,7 +67,7 @@ TEST_F(StopManagerTest, SetOverride) {
   auto e2 = std::make_shared<Elevator>(0, 0, 0);
 
   sm->set(f, d, e1);
-  EXPECT_EQ(e1, sm->get(f, d));
+  EXPECT_EQ(true, sm->hasStop(f, d));
 
   auto stops1 = sm->getStops(e1, d);
   EXPECT_EQ(1, stops1.size());
@@ -78,11 +78,11 @@ TEST_F(StopManagerTest, SetOverride) {
   EXPECT_EQ(stops2.end(), stops2.begin());
 
   sm->set(f, d, e2);
-  EXPECT_EQ(e2, sm->get(f, d));
+  EXPECT_EQ(true, sm->hasStop(f, d));
 
   stops1 = sm->getStops(e1, d);
-  EXPECT_EQ(0, stops1.size());
-  EXPECT_EQ(stops1.end(), stops1.begin());
+  EXPECT_EQ(1, stops1.size());
+  EXPECT_EQ(f, *stops1.begin());
 
   stops2 = sm->getStops(e2, d);
   EXPECT_EQ(1, stops2.size());
@@ -100,9 +100,9 @@ TEST_F(StopManagerTest, SetAndClear) {
   sm->set(f2, d, e);
   sm->set(f3, d, e);
 
-  EXPECT_EQ(e, sm->get(f1, d));
-  EXPECT_EQ(e, sm->get(f2, d));
-  EXPECT_EQ(e, sm->get(f3, d));
+  EXPECT_EQ(true, sm->hasStop(f1, d));
+  EXPECT_EQ(true, sm->hasStop(f2, d));
+  EXPECT_EQ(true, sm->hasStop(f3, d));
 
   auto stops = sm->getStops(e, d);
   EXPECT_EQ(3, stops.size());
@@ -110,7 +110,7 @@ TEST_F(StopManagerTest, SetAndClear) {
   EXPECT_NE(stops.end(), stops.find(f2));
   EXPECT_NE(stops.end(), stops.find(f3));
 
-  sm->clear(f2, d);
+  sm->clear(f2, e, d);
 
   stops = sm->getStops(e, d);
   EXPECT_EQ(2, stops.size());
